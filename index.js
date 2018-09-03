7c5f5e7131f21a053c7a024b3285654d21170269
blar khalid <blarharki@gmail.com>
2018-09-03 09:51:36 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-const newID = 1000000000 + oldID
+const newID = 1000000000 + oldID;
+
+      
+let ageIsValid=Number.isInteger(currentAge);
+      
+      
+      let randomNumber=Math.floor(Math.random() * 20) -1;
+      
+      
+       const flooredRandomInteger = Math.floor(randomNumber);
+
+     let randomInteger=Math.floor(flooredRandomInteger);
+
+      
+      
+let randomUserID=randomNumber+1000000000
